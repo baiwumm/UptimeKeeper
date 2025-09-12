6765b46d2907d3fd516be553ebad4090a57353fd
白雾茫茫丶 <baiwumm@foxmail.com>
2025-09-12 10:26:46 +0800

    feat: 新增 全局 FullLoading 组件

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,15 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2025-09-10 15:24:53
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2025-09-11 17:31:50
+ * @LastEditTime: 2025-09-12 10:24:33
  * @Description: 布局文件
  */
 import "./globals.css";
 
 import type { Metadata } from "next";
 import { ThemeProvider } from 'next-themes'
+
+import FullLoading from '@/components/FullLoading'; // 全局 Loading
 
 export const metadata: Metadata = {
   title: `${process.env.NEXT_PUBLIC_SITE_NAME} | ${process.env.NEXT_PUBLIC_SITE_DESCRIPTION}`,
@@ -29,8 +31,10 @@ export default function RootLayout({
       </head>
       <body className='bg-gradient-to-br from-blue-50 via-white to-purple-50 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900'>
         <ThemeProvider attribute="class" enableSystem={false}>
+          {/* 全局 Loading */}
+          <FullLoading />
+          {/* 主体内容 */}
           <div className="max-w-7xl mx-auto p-4 flex flex-col gap-6 min-h-screen">
-            {/* 主体内容 */}
             <main>
               {children}
             </main>
