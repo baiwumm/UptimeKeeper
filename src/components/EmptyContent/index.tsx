6e9002dc601a8a20596f642349031a4f7d115333
baiwumm <me@baiwumm.com>
2026-07-14 18:15:46 +0800

    style(EmptyContent): 优化 UI 展示

--- src/components/EmptyContent/index.tsx
+++ src/components/EmptyContent/index.tsx
@@ -2,34 +2,32 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2026-07-08 16:01:53
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2026-07-08 16:19:36
+ * @LastEditTime: 2026-07-14 18:14:53
  * @Description: 空数据
  */
-import { Plus } from '@gravity-ui/icons';
-import { Alert, Button } from "@heroui/react";
+import { DatabaseFill, Plus } from '@gravity-ui/icons';
+import { Button, Typography } from "@heroui/react";
 import Link from "next/link";
 import { type FC } from 'react';
 
 const EmptyContent: FC = () => {
-  const renderButton = (className: string) => (
-    <Link href='https://dashboard.uptimerobot.com/monitors' target="_blank" className={className}>
-      <Button size="sm" variant="primary" >
-        <Plus />
-        添加站点
-      </Button>
-    </Link>
-  )
   return (
-    <div className="flex-1 flex items-center justify-center">
-      <Alert status="accent" className='max-w-xl'>
-        <Alert.Indicator />
-        <Alert.Content>
-          <Alert.Title>一切安静如常 🕊️</Alert.Title>
-          <Alert.Description>目前没有站点被监控。您可以随时添加一个！</Alert.Description>
-          {renderButton("mt-2 sm:hidden")}
-        </Alert.Content>
-        {renderButton("hidden sm:block")}
-      </Alert>
+    <div className="flex-1 flex flex-col items-center justify-center">
+      <div className="flex-1 size-full max-w-xl max-h-100 border-border p-6 rounded-2xl border border-dashed text-center flex justify-center items-center">
+        <div className="flex flex-col gap-2 items-center">
+          <div className="bg-default text-foreground p-4 rounded-full">
+            <DatabaseFill className="size-5" />
+          </div>
+          <Typography type="h5">一切安静如常 🕊️</Typography>
+          <Typography type="body-sm">目前没有站点被监控。您可以随时添加一个！</Typography>
+          <Link href='https://dashboard.uptimerobot.com/monitors' target="_blank">
+            <Button size="sm" variant="primary" >
+              <Plus />
+              添加站点
+            </Button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
